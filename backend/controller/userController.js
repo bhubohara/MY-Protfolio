2de36c4cde197ff0da6497fdcddd52e6dbcb7a4a
bhu bohara <86654532+bhubohara@users.noreply.github.com>
2025-01-15 22:58:26 +0545

    Update userController.js
    updated

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -134,7 +134,7 @@ export const logout = catchAsyncErrors(async (req, res, next) => {
       expires: new Date(Date.now()),
       httpOnly: true,
       sameSite:"None",
-      success:true
+      secure:true
     })
     .json({
       success: true,
